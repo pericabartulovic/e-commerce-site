855995037beaf9596d5be1d061408823c1513873
pericabartulovic <pericab35@gmail.com>
2025-03-28 23:00:36 +0100

    prepare for prod

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -22,6 +22,11 @@ async function start() {
   
   app.use('/images', express.static(path.join(__dirname, '../assets')));
   
+  app.use(express.static(
+    path.resolve(__dirname, '../dist'),
+    { maxAge: '1y', etag: false },
+  ));
+  
   app.get('/api/products', async(req, res) => {
     const products = await db.collection('products').find({}).toArray();
     res.send(products);
@@ -72,10 +77,16 @@ async function start() {
     const user = await db.collection('users').findOne({ id: req.params.userId });
     const populatedCart = await populateCartIds(user?.cartItems || []);
     res.json(populatedCart);
-  })
+  });
   
-  app.listen(8000, () => {
-    console.log('Server is listening on port 8000')
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+  });
+  
+  const port = process.env.PORT || 8000;
+  
+  app.listen(port, () => {
+    console.log('Server is listening on port' + port);
   });
 }
 
